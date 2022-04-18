c17038cbd428bc38ec020650b5f444b9283b465b
Stefan Weiß <stefan.weiss1@aol.com>
2022-04-18 12:50:36 +0200

    Initial Commit

---
+++ Last-Udacity-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Last-Udacity-Project
+//
+//  Created by Stefan Weiss on 18.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
